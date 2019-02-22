cd4beffe7127c4e44b60bae1b256a6be913c61eb
taniaro <kesstasia@gmail.com>
2019-02-22 22:12:28 +0200

    started working with equation solver class

--- SimpleGA/SimpleGA/EqualitySolver.cs
+++ SimpleGA/SimpleGA/EqualitySolver.cs
@@ -8,6 +8,26 @@ namespace SimpleGA
 {
     class EqualitySolver
     {
+        static readonly int COEFF_COUNT = 4;
+
+        public int[] Coefficients { get; }
+        public int EquationResult { get; }
+        Gene[] population;
+
+        public EqualitySolver(int[] Coefficients, int EquationResult)
+        {
+            this.Coefficients = Coefficients;
+            this.EquationResult = EquationResult;
+        }
+
+        public void Fitness(ref Gene g)
+        {
+            int temp = 0;
+            for (int i = 0; i < COEFF_COUNT; i++)
+                temp += g.Alleles[i] * Coefficients[i];
+            g.Fitness = Math.Abs(temp - EquationResult);
+        }
+
 
     }
 }
