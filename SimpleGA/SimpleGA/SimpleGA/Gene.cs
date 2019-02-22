ebd6c008fc8e8b9bf95ef72f3ce1e334d3160c37
taniaro <kesstasia@gmail.com>
2019-02-22 21:33:27 +0200

    made gene class

---
+++ SimpleGA/SimpleGA/SimpleGA/Gene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGA
+{
+    class Gene
+    {
+        static readonly int ALLELES_COUNT = 4;
+
+        public int[] Alleles { get; set; }
+        public int Fitness { get; set; }
+
+        public static bool operator ==(Gene g1, Gene g2)
+        {
+            for (int i = 0; i < ALLELES_COUNT; i++)
+                if (g1.Alleles[i] != g2.Alleles[i])
+                    return false;
+            return true;
+        }
+
+        public static bool operator !=(Gene g1, Gene g2)
+        {
+            return !(g1 == g2);
+        }
+
+        public static bool operator >(Gene g1, Gene g2)
+        {
+            return g1.Fitness > g2.Fitness;
+        }
+
+        public static bool operator <(Gene g1, Gene g2)
+        {
+            return g1.Fitness < g2.Fitness;
+        }
+    }
+}
---
+++ SimpleGA/SimpleGA/SimpleGA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gene g1 = new Gene();
+            Gene g2 = new Gene();
+            int[] a = { 1, 2, 3, 4 };
+            int[] b = { 1, 2, 3, 4 };
+            g1.Alleles = a;
+            g2.Alleles = b;
+            g1.Fitness = 63;
+            g2.Fitness = 80;
+            if(g1 == g2)
+                Console.WriteLine("Equal genes");
+
+        }
+    }
+}
