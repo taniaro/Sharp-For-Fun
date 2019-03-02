4ed2ba5e019b64b2ca40e40168ee2b1891b0fdf8
taniaro <kesstasia@gmail.com>
2019-03-02 23:46:20 +0200

    made scheme

---
+++ BankProgram/BankLib/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLib
+{
+    public interface IAccount
+    {
+        //Receive money on account
+        void Receive(decimal sum);
+        //Withdraw money from account
+        decimal Withdraw(decimal sum);
+    }
+}
