92d405134d053c641431b3cbc24454b526744e78
taniaro <kesstasia@gmail.com>
2019-03-02 23:46:47 +0200

    made class for events' processing

---
+++ BankProgram/BankLib/AccountEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLib
+{
+    //for creation of the events
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    //for events' processing
+    public class AccountEventArgs
+    {
+        public string Message { get; private set; }
+        public decimal Sum { get; private set; }
+
+        public AccountEventArgs(string Message, decimal Sum)
+        {
+            this.Message = Message;
+            this.Sum = Sum;
+        }
+    }
+}
