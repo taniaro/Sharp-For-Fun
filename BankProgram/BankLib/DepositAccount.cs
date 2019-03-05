bd597c88f004320e7d9e194eb7b42370c9f4c521
taniaro <kesstasia@gmail.com>
2019-03-05 22:41:15 +0200

    designed class for deposit account

---
+++ BankProgram/BankLib/DepositAccount.cs
@@ -0,0 +1,53 @@
+namespace BankLib
+{
+    public class DepositAccount : Account
+    {
+        //deposit account has a period, for example 30 days
+        public readonly int PERIOD;
+
+        public DepositAccount(decimal sum, float percentage, int period) 
+            : base(sum, percentage) => PERIOD = period;
+
+        protected internal override Open()
+        {
+            base.OnOpen(new AccountEventArgs(
+                $"New account {Id} opened. Current sum: ${Sum}", Sum));
+        }
+
+        public override void Receive(decimal sum)
+        {
+            if(days%PERIOD == 0)
+            {
+                base.Receive(sum);
+            }
+            else
+            {
+                base.OnReceived(new AccountEventArgs(
+                    $"You can put money on your account {Id} only after {PERIOD}-days period." + 
+                        $"Balance: {Sum}", 0));
+            }
+        }
+
+        public override decimal Withdraw(decimal sum)
+        {
+            if(days%PERIOD == 0)
+            {
+                base.Withdraw(sum);
+            }
+            else
+            {
+                base.OnWithdrawed(
+                    $"You can withdraw money from account {Id} only after {PERIOD}-days period." +
+                        $" Balance: {Sum}", 0);
+            }
+        }
+
+        protected internal override void CalculatePercentage()
+        {
+            if(days%PERIOD == 0)
+            {
+                base.CalculatePercentage();
+            }
+        }
+    }
+}
