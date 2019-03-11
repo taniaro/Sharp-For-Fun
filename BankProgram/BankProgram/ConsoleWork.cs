c8225bb186de96b2ba80ba011dc41d103d6000b8
taniaro <kesstasia@gmail.com>
2019-03-11 22:50:32 +0200

    added class for work with console

---
+++ BankProgram/BankProgram/ConsoleWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProgram
+{
+    static class ConsoleWork
+    {
+        public static void OpenAccount() { }
+
+        public static void WithdrawFromAccount() { }
+
+        public static void ReceiveOnAccount() { }
+
+        public static void CloseAccount() { }
+
+
+        private static void OpenAccountHandler() { }
+
+        private static void WithdrawFromAccountHandler() { }
+
+        private static void ReceiveOnAccountHandler() { }
+
+        private static void CloseAccountHandler() { }
+
+    }
+}
